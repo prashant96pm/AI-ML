731b635747674fc8ed797f0782f48ebbe9a35302
Prashant Marabe <prashantmarbe@gmail.com>
2024-09-11 23:31:04 +0530

    c

--- Metropolitan_Suites/webhook.py
+++ Metropolitan_Suites/webhook.py
@@ -68,7 +68,7 @@ def no_of_bedrooms():
     max_rent=max_rent['amount']
     print("Desired maximum rent ", max_rent)
     for i in list(filtered_results):
-        if(str(i['list_price_max'])<=str(max_rent)):
+        
             count=count+1
         else:
             filtered_results.remove(i)
