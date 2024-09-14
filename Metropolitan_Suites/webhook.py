de375f02573d97b9f1162566fea85df2574b18da
Prashant Marabe <prashantmarbe@gmail.com>
2024-09-14 23:28:00 +0530

    c

--- Metropolitan_Suites/webhook.py
+++ Metropolitan_Suites/webhook.py
@@ -97,7 +97,7 @@ def pets_policy():
     print("Pets ", pets)
     for i in list(filtered_results):
         if(pets=='yes'):
-            if('pets_allowed' in i['tags']):
+            
                 count=count+1
             else:
                 filtered_results.remove(i)
