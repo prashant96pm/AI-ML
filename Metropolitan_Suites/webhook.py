0525a1136e421d8626cbadf145e53fecee3bde79
Prashant Marabe <prashantmarbe@gmail.com>
2024-09-15 20:54:30 +0530

    c

--- Metropolitan_Suites/webhook.py
+++ Metropolitan_Suites/webhook.py
@@ -111,7 +111,7 @@ def filter_by_no_of_bedrooms():
     #If there are no available apartments give the option of starting over or changing the search criteria.
     count=len(filtered_results)
     if(count==0):
-        text='Oops! Looks like there are no available apartments with the preferred requirements. You can either change the search criteria or you can start over. How would you like to continue?'
+        
 
     else:
         text='Perfect! So far I have found '+ str(count) +' apartments for you. Would you like to narrow your search further by selecting your desired maximum rent? Please say "yes" or "no".'
