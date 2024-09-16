d8f904411843107a70a8273cd75a204681faeff9
Prashant Marabe <prashantmarbe@gmail.com>
2024-09-16 23:25:55 +0530

    c

--- Metropolitan_Suites/webhook.py
+++ Metropolitan_Suites/webhook.py
@@ -155,7 +155,7 @@ def filter_by_no_of_bathrooms():
     no_of_bathrooms()
     #If there are no available apartments give the option of starting over or changing the search criteria.
     count=len(filtered_results)
-    
+    if(count==0):
         text= 'Oops! Looks like there are no available apartments with the preferred requirements. You can either change the search criteria or you can start over. How would you like to continue?'
 
     else:
