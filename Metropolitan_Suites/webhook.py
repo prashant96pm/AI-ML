099de3b9b9fd28a2ae5749473a5171ab8477514b
Prashant Marabe <prashantmarbe@gmail.com>
2024-09-16 23:22:15 +0530

    c

--- Metropolitan_Suites/webhook.py
+++ Metropolitan_Suites/webhook.py
@@ -140,7 +140,7 @@ def filter_by_max_rent():
     fulfillmentResponse = {
         'fulfillment_response': {
             'messages': [{
-                'text': {
+                
                     'text': [text]
                 }
             }]
