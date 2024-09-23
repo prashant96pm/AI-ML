68844bed24d8d83aade9b47f58fcdc7ff65e4b13
Prashant Marabe <prashantmarbe@gmail.com>
2024-09-23 18:03:14 +0530

    c

--- Metropolitan_Suites/webhook.py
+++ Metropolitan_Suites/webhook.py
@@ -47,7 +47,7 @@ def search_by_zipcode():
     
 def no_of_bedrooms():
     global filtered_results
-    
+    filtered_results=[]
     count=0
     bedrooms=request.json['sessionInfo']['parameters']['bedrooms']
     bedrooms=str(bedrooms)
