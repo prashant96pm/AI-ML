45ec253cfc0b44631ee7f76001e592e281239b5c
Prashant Marabe <prashantmarbe@gmail.com>
2024-09-27 22:45:37 +0530

    c

--- Metropolitan_Suites/webhook.py
+++ Metropolitan_Suites/webhook.py
@@ -171,7 +171,7 @@ def filter_by_no_of_bathrooms():
     }
     return fulfillmentResponse
 
-
+def filter_by_pets_policy():
     #Filter out the API results based on the number of bedrooms, max rent, bathrooms and pets policy
     no_of_bedrooms()
     max_rent()
