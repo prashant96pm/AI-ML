11bbd49212eb514f6fdfbfa68d5aba7cebd1c3ad
Prashant Marabe <prashantmarbe@gmail.com>
2024-09-30 21:47:30 +0530

    c

--- Metropolitan_Suites/webhook.py
+++ Metropolitan_Suites/webhook.py
@@ -247,7 +247,7 @@ def webhook():
         resp=filter_by_no_of_bathrooms()
         return jsonify(resp)
     
-    if(tag=='filter-by-pets-policy'):
+    
         filter_by_pets_policy()   
         
     if(tag=='send-sms'):
