80bbf7b63539b60c57d05dc42fd21664cd72fba0
Prashant Marabe <prashantmarbe@gmail.com>
2024-11-24 09:02:29 +0530

    c

--- Amazon_Bedrock_ChatBot_Img_txt_API/imageApi/py/py/py_stack.py
+++ Amazon_Bedrock_ChatBot_Img_txt_API/imageApi/py/py/py_stack.py
@@ -8,7 +8,7 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-class PyStack(Stack):
+
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
