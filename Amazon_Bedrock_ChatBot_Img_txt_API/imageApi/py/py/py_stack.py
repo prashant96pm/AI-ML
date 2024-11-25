a9bd10d1c68e63f90fab37b3628748c3deccc10c
Prashant Marabe <prashantmarbe@gmail.com>
2024-11-25 18:42:25 +0530

    c

--- Amazon_Bedrock_ChatBot_Img_txt_API/imageApi/py/py/py_stack.py
+++ Amazon_Bedrock_ChatBot_Img_txt_API/imageApi/py/py/py_stack.py
@@ -31,7 +31,7 @@ class PyStack(Stack):
 
         images_lambda.add_to_role_policy(aws_iam.PolicyStatement(
             effect=aws_iam.Effect.ALLOW,
-            
+            resources=["*"],
             actions=["bedrock:InvokeModel"]
         ))
 
