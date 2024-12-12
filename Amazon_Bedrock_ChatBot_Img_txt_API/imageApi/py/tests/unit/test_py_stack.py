0603326a86287ab2d774e9b52f3fc39a51dc3fc7
Prashant Marabe <prashantmarbe@gmail.com>
2024-12-12 15:36:10 +0530

    c

--- Amazon_Bedrock_ChatBot_Img_txt_API/imageApi/py/tests/unit/test_py_stack.py
+++ Amazon_Bedrock_ChatBot_Img_txt_API/imageApi/py/tests/unit/test_py_stack.py
@@ -1,7 +1,7 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from py.py_stack import PyStack
+
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in py/py_stack.py
