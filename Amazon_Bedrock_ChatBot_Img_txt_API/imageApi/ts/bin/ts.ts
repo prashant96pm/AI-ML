dc0118b3de03b2ddfe6bc7cfc236d81c7e16df10
Prashant Marabe <prashantmarbe@gmail.com>
2025-01-08 08:59:31 +0530

    c

--- Amazon_Bedrock_ChatBot_Img_txt_API/imageApi/ts/bin/ts.ts
+++ Amazon_Bedrock_ChatBot_Img_txt_API/imageApi/ts/bin/ts.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { TsStack } from '../lib/ts-stack';
 
-
+const app = new cdk.App();
 new TsStack(app, 'TsImageStack', {
 
 });
