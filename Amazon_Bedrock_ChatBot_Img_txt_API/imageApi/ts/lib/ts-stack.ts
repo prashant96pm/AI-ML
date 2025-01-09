caa4c7f5a01338ec5c458a7ba5f032f10cec0700
Prashant Marabe <prashantmarbe@gmail.com>
2025-01-09 12:43:32 +0530

    c

--- Amazon_Bedrock_ChatBot_Img_txt_API/imageApi/ts/lib/ts-stack.ts
+++ Amazon_Bedrock_ChatBot_Img_txt_API/imageApi/ts/lib/ts-stack.ts
@@ -1,4 +1,4 @@
-
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Bucket } from 'aws-cdk-lib/aws-s3'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
