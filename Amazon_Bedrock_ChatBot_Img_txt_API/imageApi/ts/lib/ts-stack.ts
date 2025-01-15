505f64565af3a814f990f29da200000fcbbbc879
Prashant Marabe <prashantmarbe@gmail.com>
2025-01-15 12:05:19 +0530

    c

--- Amazon_Bedrock_ChatBot_Img_txt_API/imageApi/ts/lib/ts-stack.ts
+++ Amazon_Bedrock_ChatBot_Img_txt_API/imageApi/ts/lib/ts-stack.ts
@@ -7,7 +7,7 @@ import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { LambdaIntegration, ResourceOptions, RestApi } from 'aws-cdk-lib/aws-apigateway';
 
-export class TsStack extends cdk.Stack {
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
