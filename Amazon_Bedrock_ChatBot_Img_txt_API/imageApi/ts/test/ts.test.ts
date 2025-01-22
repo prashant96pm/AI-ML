c64629c9d0d88dc088248b6a6a2b7977f0c3b0c6
Prashant Marabe <prashantmarbe@gmail.com>
2025-01-22 18:18:52 +0530

    c

--- Amazon_Bedrock_ChatBot_Img_txt_API/imageApi/ts/test/ts.test.ts
+++ Amazon_Bedrock_ChatBot_Img_txt_API/imageApi/ts/test/ts.test.ts
@@ -2,7 +2,7 @@
 // import { Template } from 'aws-cdk-lib/assertions';
 // import * as Ts from '../lib/ts-stack';
 
-// example test. To run these tests, uncomment this file along with the
+
 // example resource in lib/ts-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
